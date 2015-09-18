85c1330ba89858e85eb7a40bf3c86ac048bc8e61
SimonLegaultHebert <simon.legault.hebert@gmail.com>
2015-09-18 17:40:19 -0400

    refactoring of the CreationCommande logic

--- Service_Commande/src/service/forms/CreationCommandeForm.java
+++ Service_Commande/src/service/forms/CreationCommandeForm.java
@@ -1,5 +1,111 @@
 package service.forms;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import service.beans.Client;
+import service.beans.Commande;
+
 public class CreationCommandeForm {
 
+	private static final String FORMAT_DATE = "dd/MM/yyyy HH:mm:ss";
+	private static final String CHAMP_DATE = "dateCommande";
+	private static final String CHAMP_MONTANT = "montantCommande";
+	private static final String CHAMP_MODE_PAIEMENT = "modePaiementCommande";
+	private static final String CHAMP_STATUT_PAIEMENT = "statutPaiementCommande";
+	private static final String CHAMP_MODE_LIVRAISON = "modeLivraisonCommande";
+	private static final String CHAMP_STATUT_LIVRAISON = "statutLivraisonCommande";
+	private static final Commande COMMANDE = new Commande();
+	
+	private String resultat;
+	private HashMap<String, String> erreurs = new HashMap<String, String>();
+	
+	public Commande creerCommande(HttpServletRequest req){
+		CreationClientForm clientForm = new CreationClientForm();
+		Client client = clientForm.creerClient(req);
+		erreurs = clientForm.getErreurs();
+		COMMANDE.setClient(client);
+		
+		validationModePaiement(req.getParameter(CHAMP_MODE_PAIEMENT).trim());
+		validationStatutPaiement(req.getParameter(CHAMP_STATUT_PAIEMENT).trim());
+		validationModeLivraison(req.getParameter(CHAMP_MODE_LIVRAISON).trim());
+		validationStatutLivraison(req.getParameter(CHAMP_STATUT_LIVRAISON).trim());
+		validationMontant(req.getParameter(CHAMP_MONTANT).trim());
+		creerDate();
+		if (erreurs.isEmpty()) {
+            resultat = "Succès de la création de la commande.";
+        } else {
+            resultat = "Échec de la création de la commande.";
+        }
+		return COMMANDE;
+	}
+
+	private void validationModePaiement(String modePaiement){
+		if(modePaiement == null || modePaiement.length() < 2){
+			rajouterErreur(CHAMP_MODE_PAIEMENT, "Le mode de paiement doit contenir au moins 2 caractères.");
+		}
+		COMMANDE.setModePaiement(modePaiement);
+	}
+	
+	private void validationStatutPaiement(String statutPaiement){
+		if(statutPaiement.length() < 2){
+			rajouterErreur(CHAMP_STATUT_PAIEMENT, "Le statut de paiement doit contenir au moins 2 caractères.");
+		}
+		COMMANDE.setStatutPaiement(statutPaiement);
+	}	
+	
+	private void validationModeLivraison(String modeLivraison){
+		if(modeLivraison == null || modeLivraison.length() < 2){
+			rajouterErreur(CHAMP_MODE_LIVRAISON, "Le mode de paiement doit contenir au moins 2 caractères.");		
+		}
+		COMMANDE.setModeLivraison(modeLivraison);
+	}
+	
+	private void validationStatutLivraison(String statutLivraison){
+		if(statutLivraison.length() < 2){
+			rajouterErreur(CHAMP_STATUT_LIVRAISON, "Le statut de livraison doit contenir au moins 2 caractères.");
+		}
+		COMMANDE.setStatutLivraison(statutLivraison);
+	}
+	
+	private void validationMontant(String montant){
+		double dMontant = -1;
+		if(montant == null){
+			rajouterErreur(CHAMP_MONTANT, "Le montant ne doit pas être vide.");
+		}else{
+			try{
+				dMontant = Double.parseDouble(montant);	
+			}catch(NumberFormatException e){
+				rajouterErreur(CHAMP_MONTANT, "Le montant doit contenir seulement des chiffres.");
+			}
+		}		
+		COMMANDE.setMontant(dMontant);
+	}
+	
+	private void creerDate(){
+		DateFormat dateFormat = new SimpleDateFormat(FORMAT_DATE);
+		Date date = new Date(); 
+		COMMANDE.setDate(dateFormat.format(date));
+	}
+	
+	private void rajouterErreur(String champ, String message){
+		erreurs.put(champ, message);
+	}
+	
+	public String getResultat() {
+		return resultat;
+	}
+
+	public HashMap<String, String> getErreurs() {
+		return erreurs;
+	}
+
+	
+
+	
+	
 }
--- Service_Commande/src/service/servlets/CreationCommande.java
+++ Service_Commande/src/service/servlets/CreationCommande.java
@@ -12,78 +12,36 @@ import javax.servlet.http.HttpServletResponse;
 
 import service.beans.Client;
 import service.beans.Commande;
+import service.forms.CreationCommandeForm;
 
 public class CreationCommande extends HttpServlet {
-	
-	public static final String CHAMP_NOM              = "nomClient";
-    public static final String CHAMP_PRENOM           = "prenomClient";
-    public static final String CHAMP_ADRESSE          = "adresseClient";
-    public static final String CHAMP_TELEPHONE        = "telephoneClient";
-    public static final String CHAMP_EMAIL            = "emailClient";
+
  
-    public static final String CHAMP_DATE             = "dateCommande";
-    public static final String CHAMP_MONTANT          = "montantCommande";
-    public static final String CHAMP_MODE_PAIEMENT    = "modePaiementCommande";
-    public static final String CHAMP_STATUT_PAIEMENT  = "statutPaiementCommande";
-    public static final String CHAMP_MODE_LIVRAISON   = "modeLivraisonCommande";
-    public static final String CHAMP_STATUT_LIVRAISON = "statutLivraisonCommande";
- 
-    public static final String ATT_COMMANDE           = "commande";
-    public static final String ATT_MESSAGE            = "message";
-    public static final String ATT_ERREUR             = "erreur";
- 
-    public static final String FORMAT_DATE            = "dd/MM/yyyy HH:mm:ss";
- 
+    public static final String ATT_FORM = "form";
+    public static final String ATT_COMMANDE = "commande";
+
     public static final String VUE_FORM = "/WEB-INF/creerCommande.jsp";
-	
-//	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-//		String message = "Commande créée avec succès !";
-//		boolean erreur = false;
-//		Commande commande = createCommandeWithRequestValues(req);
-//		Client client = createClientWithRequestValues(req);
-//		
-//		if(!client.isClientValid()|| !commande.isCommandeValid()){
-//			erreur = true;
-//			message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"creerCommande.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'une commande.";
-//		}
-//		req.setAttribute(ATT_ERREUR, erreur);
-//		req.setAttribute(ATT_COMMANDE, commande);
-//		req.setAttribute(ATT_MESSAGE, message);
-//		this.getServletContext().getRequestDispatcher(VUE).forward(req, resp);
-//	}
+    public static final String VUE_SUCCES = "/WEB-INF/afficherCommande.jsp";
     
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
     	this.getServletContext().getRequestDispatcher(VUE_FORM).forward(req, resp);
     }
-	
-	private Commande createCommandeWithRequestValues(HttpServletRequest req){
-		Commande commande = new Commande();
-		DateFormat dateFormat = new SimpleDateFormat(FORMAT_DATE);
-		Date date = new Date(); 
-		commande.setDate(dateFormat.format(date));
-		double montant;
-		try{
-			montant = Double.parseDouble(req.getParameter(CHAMP_MONTANT).trim());	
-		}catch(NumberFormatException e){
-			montant = -1;
+    
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+    	CreationCommandeForm form = new CreationCommandeForm();
+    	Commande commande = form.creerCommande(req);
+    	req.setAttribute(ATT_COMMANDE, commande);
+    	req.setAttribute(ATT_FORM, form);
+    	
+    	if(form.getErreurs().isEmpty()){
+			this.getServletContext().getRequestDispatcher(VUE_SUCCES).forward(req, resp);
+		}else{
+			this.getServletContext().getRequestDispatcher(VUE_FORM).forward(req, resp);
 		}
-		commande.setMontant(montant);
-		commande.setModeLivraison(req.getParameter(CHAMP_MODE_LIVRAISON).trim());
-		commande.setModePaiement(req.getParameter(CHAMP_MODE_PAIEMENT).trim());
-		commande.setModeLivraison(req.getParameter(CHAMP_MODE_LIVRAISON).trim());
-		commande.setStatutLivraison(req.getParameter(CHAMP_STATUT_LIVRAISON).trim());		
-		return commande;	
-	}
+    }
 	
-	private Client createClientWithRequestValues(HttpServletRequest req){
-		Client client = new Client();
-		client.setNom(req.getParameter(CHAMP_NOM).trim());
-		client.setPrenom(req.getParameter(CHAMP_PRENOM ).trim());
-		client.setAdresse(req.getParameter(CHAMP_ADRESSE).trim());
-		client.setNumeroTelephone(req.getParameter(CHAMP_TELEPHONE).trim());
-		client.setEmail(req.getParameter(CHAMP_EMAIL).trim());		
-		return client;
-	}
+
+	
 
 	
 }
