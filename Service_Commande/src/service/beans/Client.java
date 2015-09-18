660dfdfe37a9b08fdd286f7afe665e57b4e7b34a
SimonLegaultHebert <simon.legault.hebert@gmail.com>
2015-09-18 13:24:29 -0400

    quick fix in the beans

--- Service_Commande/src/service/beans/Client.java
+++ Service_Commande/src/service/beans/Client.java
@@ -37,65 +37,6 @@ public class Client {
 	}
 	public void setEmail(String email) {
 		this.email = email;
-	}
-	
-	public boolean isClientValid(){
-		boolean isValid = true;
-		if(this.nom.isEmpty() || this.adresse.isEmpty() || this.numeroTelephone.isEmpty()){
-			isValid = false;
-		}
-		return isValid;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
-		result = prime * result + ((numeroTelephone == null) ? 0 : numeroTelephone.hashCode());
-		result = prime * result + ((prenom == null) ? 0 : prenom.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Client other = (Client) obj;
-		if (adresse == null) {
-			if (other.adresse != null)
-				return false;
-		} else if (!adresse.equals(other.adresse))
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (nom == null) {
-			if (other.nom != null)
-				return false;
-		} else if (!nom.equals(other.nom))
-			return false;
-		if (numeroTelephone == null) {
-			if (other.numeroTelephone != null)
-				return false;
-		} else if (!numeroTelephone.equals(other.numeroTelephone))
-			return false;
-		if (prenom == null) {
-			if (other.prenom != null)
-				return false;
-		} else if (!prenom.equals(other.prenom))
-			return false;
-		return true;
-	}
-	
-	
-	
+	}	
 	
 }
